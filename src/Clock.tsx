eb5e22e44cbf48f2185887433ba64861601e69c0
WWWolverine <tevrizovvv@gmail.com>
2022-08-30 07:13:48 +0600

    clock

---
+++ src/Clock.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+import "./App.css";
+
+function Clock() {
+  const [hour, setHour] = useState("");
+  const [minutes, setMinutes] = useState("");
+  const [seconds, setSeconds] = useState("");
+  const deg = 6;
+
+  const clock = () => {
+    let day = new Date();
+
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    setHour(`rotateZ(${hh + mm / 12}deg)`);
+    setMinutes(`rotateZ(${mm}deg)`);
+    setSeconds(`rotateZ(${ss}deg)`);
+  };
+  setInterval(clock, 1000);
+  return (
+    <div className="clock">
+      <div className="hour">
+        <div className="hr" style={{ transform: hour }} id="hr"></div>
+      </div>
+      <div className="min">
+        <div style={{ transform: minutes }} className="mn" id="mn"></div>
+      </div>
+      <div className="sec">
+        <div className="sc" style={{ transform: seconds }} id="sc"></div>
+      </div>
+      <h1>GitHub: https://github.com/WWWolverine/Clock</h1>
+    </div>
+  );
+}
+
+export default Clock;
